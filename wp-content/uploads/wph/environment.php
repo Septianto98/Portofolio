c8855694a0f3379ec9b74d04b406dbdb19a6d78b
Septianto98 <Septianto98@users.noreply.github.com>
2021-04-15 02:19:03 +0700

    fix all

---
+++ wp-content/uploads/wph/environment.php
@@ -0,0 +1,3 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+$environment_variable = '{"themes":{"1":{"themes_url":"http:\/\/localhost\/portofolio\/wp-content\/themes","use_child_theme":false,"main":{"folder_name":"astra","mapped_name":""}}},"allowed_paths":["D:\/Budi Luhur\/portofolio\/wp-content\/themes"],"wordpress_directory":"","site_relative_path":"\/portofolio\/"}'; ?>
